6863786fe5a85cd40c0f53d7293be91502e2f56b
marwbrandao <marianawbrandao@icloud.com>
2023-07-27 01:06:11 +0100

    Update state.h

--- SA/state.h
+++ SA/state.h
@@ -7,7 +7,11 @@ typedef struct
     TU **units; // An array of pointers to units in the cluster
     int size;   // The number of units in the cluster
     int population;
+    TU **border_units;  // The indices of the border units
+    int num_border_units; // The number of border units 
+    int id;
 } Cluster;
+
 
 int _pop_ideal(int d);
 void popul_test1(TU **units, int n, int k, int ideal_pop);
@@ -17,7 +21,9 @@ int energy_compactness(Cluster *clusters, int k);
 int
 energy(TU** units, Cluster* clusters, int margin, int k, int n);
 Cluster** first_cluster(TU **units, int k, int n);
-void change_unit(Cluster *clusters, TU **units, int k, int n);
+
+void change_unit1(Cluster *clusters, TU **units, int k, int n);
+void change_unit(Cluster *clusters, TU **units, int k, int n, int ideal_pop);
 /* Execute a random transition
    Within the bound ell */
 Cluster** /* Return whether it was successfull */
